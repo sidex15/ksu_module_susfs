d3bd4c7820f69b0b42780b3dbcd9bebd53203333
sidex15 <moses.limbo@gmail.com>
2024-10-19 11:57:44 +0800

    initial commit

---
+++ post-mount.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+MODDIR=/data/adb/modules/susfs4ksu
+
+SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
+
+source ${MODDIR}/utils.sh
+
+#modules for mounting system
+${SUSFS_BIN} add_sus_mount /system
+
+#modules for mounting /product
+${SUSFS_BIN} add_sus_mount /product
