3be9de90a9df0654ee63476400ee62918ced1274
sidex15 <moses.limbo@gmail.com>
2024-10-21 12:50:18 +0800

    Add a script that checks if YouTube Revanced is mounted 15 times

--- boot-completed.sh
+++ boot-completed.sh
@@ -7,7 +7,12 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 source ${MODDIR}/utils.sh
 
 # hide revanced
-sleep 10s
+count=0 
+max_attempts=15 
+until grep "youtube" /proc/self/mounts || [ $count -ge $max_attempts ]; do 
+    sleep 1 
+    ((count++)) 
+done
 packages="com.google.android.youtube com.google.android.apps.youtube.music"
 hide_app () {
 	for path in $(pm path $1 | cut -d: -f2) ; do ${SUSFS_BIN} add_sus_mount $path ; ${SUSFS_BIN} add_try_umount $path 1 ; done
