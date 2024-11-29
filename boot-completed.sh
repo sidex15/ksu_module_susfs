2edadc47dd4c2f50bc46c00730bd874421d62185
sidex15 <moses.limbo@gmail.com>
2024-11-29 20:57:48 +0800

    change to 'susfs:' string on susfs detection

--- boot-completed.sh
+++ boot-completed.sh
@@ -6,7 +6,7 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 
 source ${MODDIR}/utils.sh
 
-if dmesg | grep -q "susfs_init"; then
+if dmesg | grep -q "susfs:"; then
  description="description=status: ✅ SuS ඞ "
  else
  description="description=status: failed ❌ - Make sure you're on a SuSFS patched kernel!"
