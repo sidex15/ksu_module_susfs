f5262c1a8339ccb7c951a7bc7f1a70d2754f7b0c
sidex15 <moses.limbo@gmail.com>
2024-12-13 17:21:20 +0800

    Scripts: Remove the webroot folder if SUSFS is not supported in the kernel

--- boot-completed.sh
+++ boot-completed.sh
@@ -17,6 +17,7 @@ if [ -f $tmpfolder/logs/susfs_active ] ; then
 	description="description=status: ✅ SuS ඞ "
 else
 	description="description=status: failed 💢 - Make sure you're on a SuSFS patched kernel! 😭"
+	rm -rf ${MODDIR}/webroot
 	touch ${MODDIR}/disable
 fi
 sed -i "s/^description=.*/$description/g" $MODDIR/module.prop
