83d66a58d2a6cec40bca17cc72db2b458757c8a1
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-25 19:58:02 +0800

    scripts: move sus_path routine to post-mount

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -48,13 +48,6 @@ enable_sus_su_mode_1(){
 # uncomment it below to enable sus_su with mode 1 #
 #enable_sus_su_mode_1
 
-# to add paths
-# echo "/system/addon.d" >> /data/adb/susfs4ksu/sus_path.txt
-# this'll make it easier for the webui to do stuff
-for i in $(grep -v "#" $PERSISTENT_DIR/sus_path.txt); do
-	${SUSFS_BIN} add_sus_path $i && echo "[sus_path]: susfs4ksu/post-fs-data $i" >> $logfile1
-done
-
 # LSPosed
 # but this is probably not needed if auto_sus_bind_mount is enabled
 [ $force_hide_lsposed = 1 ] && {
