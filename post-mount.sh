8f322ae7733bdf15f2a793c3e2c4a370ed0b9e1d
sidex15 <24408329+sidex15@users.noreply.github.com>
2024-12-30 10:34:25 +0800

    scripts/post-mount: add logging

--- post-mount.sh
+++ post-mount.sh
@@ -3,12 +3,14 @@ MODDIR=/data/adb/modules/susfs4ksu
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 source ${MODDIR}/utils.sh
 PERSISTENT_DIR=/data/adb/susfs4ksu
+tmpfolder=/debug_ramdisk/susfs4ksu
+logfile="$tmpfolder/logs/susfs.log"
 
 # to add mounts
 # echo "/system" >> /data/adb/susfs4ksu/sus_mount.txt
 # this'll make it easier for the webui to do stuff
 for i in $(grep -v "#" $PERSISTENT_DIR/sus_mount.txt); do
-	${SUSFS_BIN} add_sus_mount $i
+	${SUSFS_BIN} add_sus_mount $i && echo "susfs4ksu/post-mount: adding sus_mount $i" >> $logfile
 done
 
 # EOF
