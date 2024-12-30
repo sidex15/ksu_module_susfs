53b28dc4d8b3153308cf9f0d875c836af1233f62
sidex15 <24408329+sidex15@users.noreply.github.com>
2024-12-30 10:32:04 +0800

    Hide Custom ROM and gapps related paths

--- boot-completed.sh
+++ boot-completed.sh
@@ -6,14 +6,20 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 
 source ${MODDIR}/utils.sh
 
- if dmesg | grep -q "susfs"; then
+if dmesg | grep -q "susfs"; then
  description="description=status: ✅ SuS ඞ "
  else
- description="description=status: failed ❌ - Make sure you're on a SuSFS patched kernel! "
+ description="description=status: failed ❌ - Make sure you're on a SuSFS patched kernel!"
  touch ${MODDIR}/disable
 fi
 
 sed -i "s/^description=.*/$description/g" $MODDIR/module.prop
+
+# hide lineage (almost all custom roms have this)
+for i in $(find /system /vendor /system_ext /product -iname *lineage* -o -name *crdroid* ) ; do ${SUSFS_BIN} add_sus_path $i ; done
+
+# hide gapps installation traces
+for i in $(find /system /vendor /system_ext /product -iname *gapps*xml -o -type d -iname *gapps*) ; do ${SUSFS_BIN} add_sus_path $i ; done
 
 # hide revanced
 count=0 
