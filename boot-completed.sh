1f2656d7cb4e0e42b75e032c96c463c152bfbb0f
sidex15 <24408329+sidex15@users.noreply.github.com>
2024-12-30 10:34:13 +0800

    detect if there's a susfs in the kernel

--- boot-completed.sh
+++ boot-completed.sh
@@ -7,9 +7,9 @@ SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 source ${MODDIR}/utils.sh
 
  if dmesg | grep -q "susfs"; then
- description="status: SUS ඞ "
+ description="description=status: ✅ SuS ඞ "
  else
- description="status: failed ❌ - Make sure you're on a SuSFS patched kernel! "
+ description="description=status: failed ❌ - Make sure you're on a SuSFS patched kernel! "
  touch ${MODDIR}/disable
 fi
 
