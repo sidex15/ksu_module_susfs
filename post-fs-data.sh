e62d223b531dc66fc23f27b208ecc5b6ab666f81
sidex15 <24408329+sidex15@users.noreply.github.com>
2024-12-30 10:34:45 +0800

    scripts/post-fs-data: offer an override for susfs activation
    its just hard to support people with varying setups really
    if youre a susfs user, I fucking hope you can open termux and
    atleast do
    ```
    touch /data/adb/susfs4ksu/susfs_force_override
    ```
    
    try to solve https://github.com/sidex15/ksu_module_susfs/issues/20

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -17,6 +17,12 @@ else
 	dmesg | grep -q "susfs:" > /dev/null && touch $tmpfolder/logs/susfs_active
 fi
 
+# for people that is on legacy with broken dmesg or disabled logging
+# actually first, fuck you
+# second, heres your override
+# touch /data/adb/susfs4ksu/susfs_force_override
+[ -f $PERSISTENT_DIR/susfs_force_override ] && touch $tmpfolder/logs/susfs_active
+
 force_hide_lsposed=0
 [ -f $PERSISTENT_DIR/config.sh ] && source $PERSISTENT_DIR/config.sh
 
