5e9c1118d3f82623ec5cf13ee4a366bcd8825ff6
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-01-16 16:14:18 +0800

    move tmpfolder location to /data/adb/ksu/susfs4ksu

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import Fade from './fade.js';
 import './space.js';
 
 //module location
-const tmpfolder="/debug_ramdisk/susfs4ksu"
+const tmpfolder="/data/adb/ksu/susfs4ksu"
 const moddir="/data/adb/modules/susfs4ksu"
 const config="/data/adb/susfs4ksu"
 const susfs_bin="/data/adb/ksu/bin/ksu_susfs"
